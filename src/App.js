3710e169d8057a91e77d5e4ffd423df39069ef4f
Balázs Izsóf <balazs.izsof@liferay.com>
2022-06-03 16:56:24 +0200

    w1-s1
    Add a number input with a label, Euros.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <label>Euros</label>
+       <p></p>
+      <input type="number" id="euro-input"/>
     </div>
   );
 }
