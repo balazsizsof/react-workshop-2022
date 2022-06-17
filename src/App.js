721e3d60578afeed13886fa4af9783be96a044c5
Balázs Izsóf <balazs.izsof@liferay.com>
2022-06-17 12:28:30 +0200

    w1-s5
    Add a second, read-only <Amount /> component that shows  instead of Euros; use exchangeRate()

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 
+function exchangeRate() {
+  return Math.random() * 10000;
+}
+
 function App() {
   return (
     <div className="App">
-      <Amount name="Euros" defaultValue={0} />
-      <Amount name="- Euros" defaultValue={-2} />
+      <Amount name="$BTC" defaultValue={exchangeRate()} inputType="text" isReadonly="true"/>
+      <Amount name="Euros" defaultValue={0} inputType="number" isReadonly="false" />
+      <p></p>
     </div>
   );
 }
 
-function Amount({ name = 'Oh... this field has no name :(', defaultValue}) {
-    const [inputClassName, setInputClassName] = useState(defaultValue < 0 ? 'negative' : 'positive');
+function Amount({ name = 'Oh... this field has no name :(', defaultValue , inputType, isReadonly}) {
+  const [inputClassName, setInputClassName] = useState(defaultValue < 0 ? 'negative' : 'positive');
 
-    const onChangeHandler = (event) => event.target.value < 0 ? setInputClassName("negative") : setInputClassName("positive")
-    return (
-      <div className="Amount">
-        <label>{name}</label>
-        <p></p>
-        <input
-          onChange={onChangeHandler} type="number" defaultValue={defaultValue} className={`amount-input-${inputClassName}`}
-        />
-      </div>
-    );
-  }
+  const onChangeHandler = (event) => event.target.value < 0 ? setInputClassName("negative") : setInputClassName("positive")
+  return (
+    <div className="Amount">
+      <label>{name}:</label><br></br>
+      <input
+        onChange={onChangeHandler} type={inputType} defaultValue={defaultValue} className={`amount-input-${inputClassName}`} readOnly={isReadonly}
+      />
+    </div>
+  );
+}
 
 export default App;
