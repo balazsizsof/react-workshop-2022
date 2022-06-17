fda166a44a7c38f2661d306530c4eee9c3aae625
Balázs Izsóf <balazs.izsof@liferay.com>
2022-06-17 13:23:14 +0200

    w1-s6

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 
-function exchangeRate() {
+function getExchangeRate() {
   return Math.random() * 10000;
 }
 
 function App() {
+  const [exchangeRate, setExchangeRate] = useState(getExchangeRate());
+  const [eurosValue, setEurosValue] = useState(0)
+
+  useEffect(() => {
+		let timer = setTimeout(()=>{setExchangeRate(0)}, 5000);
+
+		return () => {
+			clearTimeout(timer);
+		}
+	}, [eurosValue]);
+
+  const onChangeHandler = (event) => {
+    setEurosValue(event.currentTarget.value)
+  }
+
+
   return (
     <div className="App">
-      <Amount name="$BTC" defaultValue={exchangeRate()} inputType="text" isReadonly="true"/>
-      <Amount name="Euros" defaultValue={0} inputType="number" isReadonly="false" />
-      <p></p>
+      <Amount name="$BTC" value={exchangeRate} inputType="text" isReadonly={true} />
+      <Amount name="Euros" value={eurosValue} inputType="number" isReadonly={false} onChange={onChangeHandler}/>
     </div>
   );
 }
 
-function Amount({ name = 'Oh... this field has no name :(', defaultValue , inputType, isReadonly}) {
-  const [inputClassName, setInputClassName] = useState(defaultValue < 0 ? 'negative' : 'positive');
+function Amount({ name = 'Oh... this field has no name :(', value , inputType, isReadonly, onChange}) {
+  const [inputClassName, setInputClassName] = useState(value < 0 ? 'negative' : 'positive');
 
-  const onChangeHandler = (event) => event.target.value < 0 ? setInputClassName("negative") : setInputClassName("positive")
+  const onChangeHandler = (event) => {
+    event.currentTarget.value < 0 ? setInputClassName("negative") : setInputClassName("positive")
+    onChange(event)
+  }
+
   return (
     <div className="Amount">
       <label>{name}:</label><br></br>
       <input
-        onChange={onChangeHandler} type={inputType} defaultValue={defaultValue} className={`amount-input-${inputClassName}`} readOnly={isReadonly}
+        onChange={onChangeHandler} type={inputType} value={value} className={`amount-input-${inputClassName}`} readOnly={isReadonly}
       />
     </div>
   );
